d6d80c6b0d95a662d4599f8f5b9e60038e9c84bc
Ashley Anderton <ashley.j.anderton@gmail.com>
2020-06-25 15:04:33 +0100

    All current zendesk updates

--- Frontend/src/js/custom.js
+++ Frontend/src/js/custom.js
@@ -3,13 +3,46 @@ $("body").addClass("govuk-template__body js-enabled");
 $(".tl-breadcrumbs li:first-child a").text("Home");
 
 
-//Prevents T Levels from wrappings
+//Prevents T Levels from wrapping
 $(document).ready(function () {
     $('h1, h1 a, h2, h2 a, h3, h3 a:contains("T Level")').contents().each(function () {
         if (this.nodeType == 3) {
             $(this).parent().html(function (_, oldValue) {
                 return oldValue.replace(/T Level/g, "<span class='tl-nowrap'>$&</span>")
             })
+        }
+    });
+});
+
+// Article voting show/hide
+$(document).ready(function () {
+    $(".tl-article--vote--button").click(function () {
+        event.preventDefault();
+        $(".tl-article--vote--buttons").addClass("tl-hidden");       
+        $(".tl-article--vote--question").text("Thank you for your feedback");
+
+        
+    });
+});
+
+
+// Search error messaging
+
+var searchbox = $(".tl-search--container input[type=search]");
+
+function showerror(message) {
+    $(".tl-search--error").removeClass("tl-hidden");   
+    $(".tl-form-group").addClass("tl-form-group--error");
+    $(".tl-search #query").addClass("tl-input--error");
+    $(".tl-error--message").text(message);
+    $(".tl-input--error:visible:first").focus();
+}
+
+$(document).ready(function () {
+    $(".tl-search--container").submit(function ()  {
+        if ( !searchbox.val() ) {
+            event.preventDefault();
+            showerror("You need to enter a search term");
         }
     });
 });
